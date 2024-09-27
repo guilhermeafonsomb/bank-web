684ef8bbf2459add2309a10f959b5cc27efb9cb4
Guilherme <guilherme.bail@gauge.haus>
2024-09-27 07:51:19 -0300

    wip: fazendo componente tabs

---
+++ src/app/components/tabs/index.tsx
@@ -0,0 +1,38 @@
+import { Tab, TabGroup, TabList, TabPanel, TabPanels } from "@headlessui/react";
+import clsx from "clsx";
+
+interface TabData {
+  label: string;
+  content: React.ReactNode;
+}
+
+interface TabsProps {
+  tabs: TabData[];
+}
+
+export default function Tabs({ tabs }: TabsProps) {
+  return (
+    <TabGroup className="w-full max-w-5xl flex flex-col justify-center items-start p-4 border rounded-md">
+      <TabList className="flex justify-between w-full border-b border-gray-800">
+        {tabs.map((tab, index) => (
+          <Tab
+            className={({ selected }) =>
+              clsx(
+                "text-lg font-bold pb-2",
+                selected ? "text-blue-500" : "text-gray-20 "
+              )
+            }
+            key={index}
+          >
+            {tab.label}
+          </Tab>
+        ))}
+      </TabList>
+      <TabPanels className="mt-4">
+        {tabs.map((tab, index) => (
+          <TabPanel key={index}>{tab.content}</TabPanel>
+        ))}
+      </TabPanels>
+    </TabGroup>
+  );
+}
--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -7,6 +7,8 @@ import { useUserStore } from "../store/userStore";
 import { UserFormData } from "../shared/models/user.models";
 import { createUsers } from "../services/userServices";
 import { useEffect } from "react";
+import Tabs from "../components/tabs";
+import { Button } from "@headlessui/react";
 
 export default function Home() {
   const { setUser, getUsers, users } = useUserStore();
@@ -19,35 +21,46 @@ export default function Home() {
     createUsers(dataUser);
   };
 
+  const test = [
+    { label: "tab 1", content: <Button>Tab 1</Button> },
+    { label: "tab 2", content: <Button>Tab 2</Button> },
+    { label: "tab 3", content: <Button>Tab 3</Button> },
+  ];
+
   useEffect(() => {
     getUsers();
   }, [getUsers]);
 
   return (
-    <section className="flex justify-end">
-      <section className="flex w-full gap-4 p-4 md:max-w-96">
-        <div className="w-2/4">
-          <Modal labelButton="Criar usuário" modalTitle="Crie um usuário">
-            <FormUser onSubmitData={handleSubmitData} />
-          </Modal>
-        </div>
+    <section>
+      <section className="flex justify-end">
+        <section className="flex w-full gap-4 p-4 md:max-w-96">
+          <div className="w-2/4">
+            <Modal labelButton="Criar usuário" modalTitle="Crie um usuário">
+              <FormUser onSubmitData={handleSubmitData} />
+            </Modal>
+          </div>
 
-        <div className="w-2/4">
-          <Dropdown labelButton="Trocar de usuário">
-            <div className="flex flex-col gap-6 ">
-              {users.map((user) => (
-                <div
-                  className="w-full flex flex-col gap-2 cursor-pointer"
-                  key={user.id}
-                  onClick={() => setUser(user.cpf)}
-                >
-                  <p className="text-sm">{user.name}</p>
-                  <p className="text-xs text-slate-500">{user.cpf}</p>
-                </div>
-              ))}
-            </div>
-          </Dropdown>
-        </div>
+          <div className="w-2/4">
+            <Dropdown labelButton="Trocar de usuário">
+              <div className="flex flex-col gap-6 ">
+                {users.map((user) => (
+                  <div
+                    className="w-full flex flex-col gap-2 cursor-pointer"
+                    key={user.id}
+                    onClick={() => setUser(user.cpf)}
+                  >
+                    <p className="text-sm">{user.name}</p>
+                    <p className="text-xs text-slate-500">{user.cpf}</p>
+                  </div>
+                ))}
+              </div>
+            </Dropdown>
+          </div>
+        </section>
+      </section>
+      <section className="flex items-center justify-center w-full">
+        <Tabs tabs={test} />
       </section>
     </section>
   );
